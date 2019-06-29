e156f61ef62d0a42c98a3649c178916a383550b8
John Carew <johnwcarew@gmail.com>
2019-06-29 03:18:59 -0500

    Update publish script for AppVeyor

--- IBM.IAM.AWS.SecurityToken/PublishToGallery.ps1
+++ IBM.IAM.AWS.SecurityToken/PublishToGallery.ps1
@@ -1,5 +1,26 @@
-if (Test-Path '..\..\.Nuget.key'){
-	Publish-Module -Name 'IBM.IAM.AWS.SecurityToken' -NuGetApiKey (Get-Content -Raw '..\..\.Nuget.key')
+$Module = "IBM.IAM.AWS.SecurityToken"
+
+if ($env:APPVEYOR_BUILD_VERSION){
+	$Module = ".\IBM.IAM.AWS.SecurityToken\bin\Release\netstandard2.0\"
+	Write-Host "Updating module manifest with AppVeyor version $env:APPVEYOR_BUILD_VERSION"
+	$date = Get-Date -Format MM/dd/yyyy
+	$ModuleManifestPath = "$($Module)IBM.IAM.AWS.SecurityToken.psd1"
+	$ModuleManifest = Get-Content $ModuleManifestPath -Raw
+	$ModuleManifest = $ModuleManifest -replace "(ModuleVersion\W*=)\W*'(.*)'", "`$1 '$env:APPVEYOR_BUILD_VERSION'"
+	$ModuleManifest = $ModuleManifest -replace "(Generated on:)\W*(.*)", "`$1 $date"
+	$ModuleManifest | Out-File -LiteralPath $ModuleManifestPath
+
+	Get-Content $ModuleManifestPath -Raw
+}
+$NuGetApiKey = $null
+if ($Env:NuGetApiKey){
+	$NuGetApiKey = $Env:NuGetApiKey
+} elseif (Test-Path '..\.Nuget.key') {
+	$NuGetApiKey = (Get-Content -Raw '..\.Nuget.key')
+}
+
+if ($NuGetApiKey){
+	Publish-Module -Path $Module -NuGetApiKey $NuGetApiKey
 } else {
-	Write-Error "Nuget API key file '.Nuget.key' is missing, please create it and add one line that contains your API key for nuget."
+	Write-Error "Nuget API key is missing, please create the file and add one line that contains your API key for nuget or set the environment variable [NuGetApiKey]."
 }
